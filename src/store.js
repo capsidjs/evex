25e1e7f9cfbc0923c7f8ba65e623a7a5e5ba2d2b
Yoshiya Hinosawa <stibium121@gmail.com>
2017-12-17 23:57:48 +0900

    feat: show error message when the action execution failed

--- src/store.js
+++ src/store.js
@@ -23,6 +23,7 @@ class Triple {
       return this.module[this.key](this.module[store.key], action)
     } catch (e) {
       console.log(`action execution failed: ${this.type}`)
+      console.log(e.message)
       console.log(e)
     }
   }
